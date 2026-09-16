01b380bb15a6d2ce8c5abeed3d61a52ea3e6f411
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-09-17 02:19:22 +0400

    Support navigation visibility on pages

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -8,13 +8,14 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Page extends Model
 {
     protected $fillable = [
-        'slug', 'title', 'seo', 'is_published', 'sort_order',
+        'slug', 'title', 'seo', 'is_published', 'show_in_nav', 'sort_order',
     ];
 
     protected $casts = [
         'title' => 'array',
         'seo' => 'array',
         'is_published' => 'boolean',
+        'show_in_nav' => 'boolean',
         'sort_order' => 'integer',
     ];
 
