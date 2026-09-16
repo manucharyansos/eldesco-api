fbb7f6c712a8065ff78e81712784269cdcbd3da2
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-09-17 02:16:01 +0400

    Add hashing configuration

---
+++ config/hashing.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'driver' => 'bcrypt',
+    'bcrypt' => [
+        'rounds' => env('BCRYPT_ROUNDS', 12),
+        'verify' => true,
+        'limit' => null,
+    ],
+    'argon' => [
+        'memory' => 65536,
+        'threads' => 1,
+        'time' => 4,
+    ],
+    'rehash_on_login' => true,
+];
