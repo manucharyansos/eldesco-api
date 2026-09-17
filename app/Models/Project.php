c19624ed7faaa66d6a67ff1dbf285935ffb05a90
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-09-17 14:21:55 +0400

    Use bundled project images as demo fallbacks

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -24,12 +24,22 @@ class Project extends Model
             'id' => $this->id,
             'title' => $this->{"title_$lang"} ?? $this->title_en,
             'description' => $this->{"description_$lang"} ?? $this->description_en,
-            'image' => $this->image_url,
+            'image' => $this->image_url ?: $this->demoImage(),
             'thumbnail' => $this->thumbnail_url,
             'category' => $this->category,
             'featured' => $this->featured,
             'createdAt' => $this->created_at
         ];
+    }
+
+    private function demoImage(): ?string
+    {
+        return match ((int) $this->order_index) {
+            1 => '/images/projects/substation.png',
+            4 => '/images/projects/led.png',
+            5 => '/images/projects/metalworks.png',
+            default => null,
+        };
     }
 
     public function scopeFeatured($query)
